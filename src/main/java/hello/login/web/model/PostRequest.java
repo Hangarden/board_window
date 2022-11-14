ac1e5fd6b1b568c4082a19c9bc2118ce18a1e774
okestro <woorong98@gmail.com>
2022-11-14 14:44:09 +0900

    Post Test, xml, Dto완료 (Create)

---
+++ src/main/java/hello/login/web/model/PostRequest.java
@@ -0,0 +1,14 @@
+package hello.login.web.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostRequest {
+
+    private Long id;             // PK
+    private String title;        // 제목
+    private String content;      // 내용
+    private String writer;       // 작성자
+}
