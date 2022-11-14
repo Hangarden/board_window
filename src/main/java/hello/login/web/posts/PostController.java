24db8b25038d1a5ca2cb7c272c7996f9ffeea602
okestro <woorong98@gmail.com>
2022-11-14 21:03:30 +0900

    CRUD완료, MessageDTO, controller

--- src/main/java/hello/login/web/posts/PostController.java
+++ src/main/java/hello/login/web/posts/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
@@ -35,12 +36,13 @@ public class PostController {
 
     // 신규 게시글 생성
     @PostMapping("/posts/save.do")
-    public String savePost(final PostRequest params, HttpSession session) {
+    public String savePost(final PostRequest params, HttpSession session, Model model) {
             Object ob1 = session.getAttribute("NAME");
             String mySessionName = (String)ob1;
             params.setWriter(mySessionName);
             postService.savePost(params);
-        return "redirect:/post/list.do";
+            MessageDto message = new MessageDto("게시글 생성이 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
+        return showMessageAndRedirect(message, model);
     }
 
     // 게시글 리스트 페이지
@@ -61,18 +63,20 @@ public class PostController {
 
     // 기존 게시글 수정
     @PostMapping("/post/update.do")
-    public String updatePost(final PostRequest params, HttpSession session) {
+    public String updatePost(final PostRequest params, HttpSession session, Model model) {
         Object ob1 = session.getAttribute("NAME");
         String mySessionName = (String)ob1;
         params.setWriter(mySessionName);
         postService.updatePost(params);
-        return "redirect:/post/list.do";
+        MessageDto message = new MessageDto("게시글 수정이 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
+        return showMessageAndRedirect(message, model);
     }
 
     // 게시글 삭제
     @PostMapping("/post/delete.do")
-    public String deletePost(@RequestParam final Long id) {
+    public String deletePost(@RequestParam final Long id, Model model) {
         postService.deletePost(id);
+        MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
         return "redirect:/post/list.do";
     }
 
